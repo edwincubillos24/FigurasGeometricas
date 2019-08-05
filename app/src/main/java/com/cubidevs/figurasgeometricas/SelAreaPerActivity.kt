2d1bf7a26460dd0c394cb6ac42cdfb6ca1c9e773
Cristian Zapata Salazar <cmauricio.zapata@udea.edu.co>
2019-08-05 16:58:43 -0500

    Merge branch 'develop' of https://github.com/udeag1/FigurasGeometricas into feature_area_cuadrado
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/res/values/strings.xml

--- app/src/main/java/com/cubidevs/figurasgeometricas/SelAreaPerActivity.kt
+++ app/src/main/java/com/cubidevs/figurasgeometricas/SelAreaPerActivity.kt
@@ -1,5 +1,6 @@
 package com.cubidevs.figurasgeometricas
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -22,7 +23,8 @@ class SelAreaPerActivity : AppCompatActivity() {
 
                     }
                     1 -> {
-
+                        var intent = Intent(this, Area_cuadradoActivity::class.java)
+                        startActivity(intent)
                     }
                     2 -> {
 
