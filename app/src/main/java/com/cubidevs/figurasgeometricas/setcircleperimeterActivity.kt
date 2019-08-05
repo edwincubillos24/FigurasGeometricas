52a5f2995f4053b38ae54875b3f45f822310d6dd
SantiagoGuiral <santiagowslg@gmail.com>
2019-08-05 09:25:05 -0500

    Feature Circle Perimeter

--- app/src/main/java/com/cubidevs/figurasgeometricas/setcircleperimeterActivity.kt
+++ app/src/main/java/com/cubidevs/figurasgeometricas/setcircleperimeterActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_setcircleperimeter.*
 
 class setcircleperimeterActivity : AppCompatActivity() {
@@ -15,10 +16,15 @@ class setcircleperimeterActivity : AppCompatActivity() {
         bperimeter.setOnClickListener {
             if (inputradio != null && inputradio.length() > 0) {
                 inradio = inputradio.text.toString().toDouble()
+
+                var perimeter = 2 * 3.1416 * inradio
+                resulperimeter.text = perimeter.toString()
+            }else{
+            Toast.makeText(this,"Input a valid number", Toast.LENGTH_LONG).show()
             }
-            var perimeter = 2 * 3.1416 * inradio
-            resulperimeter.text = perimeter.toString()
 
         }
     }
+
+
 }
